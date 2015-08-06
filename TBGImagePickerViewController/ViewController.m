acafad4cb8df3b558ce9aaa5215a409761d1f14e
十驷 <ljw040426@gmail.com>
2015-08-06 18:58:15 +0800

    Initial Commit

---
+++ TBGImagePickerViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TBGImagePickerViewController
+//
+//  Created by MexiQQ on 15/8/6.
+//  Copyright (c) 2015年 MexiQQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
